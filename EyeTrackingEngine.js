73317aad1976e75509cfb96f1ef65ef99f5a133d
Dirk Schäfer <doerkys@gmail.com>
2026-03-28 09:26:05 +0100

    Add files via upload

--- EyeTrackingEngine.js
+++ EyeTrackingEngine.js
@@ -1,64 +1,79 @@
-export default class AurionEyeTrackingEngine {
+export default class EyeTrackingEngine {
   constructor(options = {}) {
     this.video = options.video;
     this.onStatus = options.onStatus || (() => {});
-    this.cameraRunning = false;
-    this.trackingRunning = false;
-    this.onStatus("Mini-Engine lebt");
-  }
-
-  isCameraRunning() {
-    return this.cameraRunning;
-  }
-
-  isTrackingRunning() {
-    return this.trackingRunning;
+    this.onGaze = options.onGaze || (() => {});
+    this.stream = null;
+    this.tracking = false;
+    this.timer = null;
+    this.t = 0;
   }
 
   async startCamera() {
-    if (!navigator.mediaDevices?.getUserMedia) {
-      throw new Error("Kamera nicht verfügbar");
-    }
+    if (this.stream) return;
 
-    const stream = await navigator.mediaDevices.getUserMedia({
+    this.stream = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: false
     });
 
-    this.video.srcObject = stream;
+    this.video.srcObject = this.stream;
     await this.video.play();
-    this.cameraRunning = true;
-    this.onStatus("Mini-Kamera an");
+    this.onStatus("Kamera läuft");
   }
 
   stopCamera() {
-    if (this.video.srcObject) {
-      this.video.srcObject.getTracks().forEach(t => t.stop());
+    this.stopTracking();
+
+    if (this.stream) {
+      this.stream.getTracks().forEach(t => t.stop());
+      this.stream = null;
     }
-    this.video.srcObject = null;
-    this.cameraRunning = false;
-    this.onStatus("Mini-Kamera aus");
+
+    if (this.video) {
+      this.video.pause();
+      this.video.srcObject = null;
+    }
+
+    this.onStatus("Kamera gestoppt");
+  }
+
+  isCameraRunning() {
+    return !!this.stream;
   }
 
   async startTracking() {
-    this.trackingRunning = true;
-    this.onStatus("Mini-Tracking an");
+    if (!this.stream) {
+      throw new Error("Kamera läuft nicht");
+    }
+
+    if (this.tracking) return;
+
+    this.tracking = true;
+    this.onStatus("Tracking läuft");
+
+    this.timer = setInterval(() => {
+      this.t += 0.06;
+
+      const x = window.innerWidth  * 0.5 + Math.cos(this.t) * 160;
+      const y = window.innerHeight * 0.5 + Math.sin(this.t * 1.4) * 110;
+
+      this.onGaze({ x, y });
+    }, 30);
   }
 
   stopTracking() {
-    this.trackingRunning = false;
-    this.onStatus("Mini-Tracking aus");
+    this.tracking = false;
+
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+
+    this.onStatus("Tracking gestoppt");
   }
 
-  calibrateDirection() {
-    return Promise.resolve();
+  isTrackingRunning() {
+    return this.tracking;
   }
-
-  confirmCalibration() {}
-
-  getCalibrationData() {
-    return null;
-  }
-
-  loadCalibration() {}
 }
